8ad66bb4142e42c351eb94e8fcfbd1a3ddcac5d2
Mohosin212601~ <mohosin2126@gmail.com>
2023-12-25 04:17:09 +0800

    checking

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -6,6 +6,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_apiKey,
     authDomain: import.meta.env.VITE_authDomain,
