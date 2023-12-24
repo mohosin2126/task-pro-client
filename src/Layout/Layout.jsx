cbc42b6d0fd5912514bec14900a98eb055d3651a
Mohosin212601~ <mohosin2126@gmail.com>
2023-12-25 03:30:40 +0800

    added footer

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Pages/Footer/Footer";
 
 const Layout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
