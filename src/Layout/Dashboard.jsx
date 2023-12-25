89208a5dc42189f28530325d3b761ececaeca882
Mohosin212601~ <mohosin2126@gmail.com>
2023-12-25 14:04:30 +0800

    set position

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -20,26 +20,26 @@ const Dashboard = () => {
   return (
     <div className="flex">
       {/* dashboard side bar */}
-      <div className="w-64 mt-20 min-h-screen">
+      <div className="w-64 mt-20 min-h-screen fixed ">
         <ul className="menu p-4">
-          <li>
+          <li className="text-lg">
             <NavLink to="/dashboard/createnewtask">
              < MdCreateNewFolder/> Create New Task
             </NavLink>
           </li>
-          <li>
+          <li  className="text-lg">
             <NavLink to="/dashboard/mytask">
               <FcPrevious/> My Task
             </NavLink>
           </li>
           {/* shared nav links */}
           <div className="divider"></div>
-          <li>
+          <li  className="text-lg">
             <NavLink to="/">
               <FaHome /> Home
             </NavLink>
           </li>
-          <li onClick={handleLogOut} className="mt-80">
+          <li onClick={handleLogOut} className="mt-80 text-lg">
             {user ? (
               <div>
                 <LuLogOut />
@@ -50,7 +50,7 @@ const Dashboard = () => {
         </ul>
       </div>
       {/* dashboard content */}
-      <div className="flex-1 p-8">
+      <div className="flex-1 p-8 ml-64 ">
         <Outlet />
       </div>
     </div>
